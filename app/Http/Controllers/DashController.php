4d35df7d2030d20245c3e2d7fbc34019191fd746
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-19 15:50:13 +0530

    dashcontroller

---
+++ app/Http/Controllers/DashController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\support\Facades\DB;
+// use DB;
+use App\Models\user;
+use App\Models\formBasic;
+use App\Http\Controllers\CustomAuthController;
+use Auth;
+
+
+class DashController extends Controller
+{
+    //view page
+    public function index()
+    {
+            $email = Auth::user()->email;
+            $data = DB::table('form_basics')->where('email',$email)->latest()->paginate(10);
+            return view('form',compact('data'));
+
+    }
+
+}
+
