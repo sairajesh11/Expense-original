f467880eb4c72a471fe5f3571115eb2d56890023
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-19 16:47:46 +0530

    font

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -85,7 +85,7 @@
         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li> -->
       <li class="nav-item">
-        <a class="navbar-brand" href="/form">Request form</a>
+        <a style="font-family: 'Arial'; font-size: 18px" class="navbar-brand" href="/form">Request form</a>
       </li>
       <li class="nav-item">
         <a class="navbar-brand" href="/contact">Receipt form</a>
@@ -133,18 +133,6 @@
             
         </div>
                 
-                <!-- <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div> -->
-
-            <!-- </div> -->
         </div>
     </body>
 </html>
