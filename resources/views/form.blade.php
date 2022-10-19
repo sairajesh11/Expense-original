e7fafe6972953e8bdabe9ef1df5bdf56f135e336
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-19 14:43:41 +0530

    nav bar

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -26,7 +26,29 @@
 
     </style>
 
-    <?php include 'http://localhost:8000/dashboard' ?>
+    <!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <a class="navbar-brand" href="/dashboard">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <!-- <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li> -->
+      <li class="nav-item">
+        <a class="nav-link" href="/form">Request form</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/contact">Receipt form</a>
+      </li>
+      <!-- <li class="nav-item">
+        <a class="nav-link " href="/expenses" >Expenses</a>
+      </li> -->
+    </ul>
+  </div>
+</nav>
 
     <div class="container">
         <br><br>
