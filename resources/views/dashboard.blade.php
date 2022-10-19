082b5097cfd01d5006ed48a9d0203b33b2c6bcdb
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-19 15:15:50 +0530

    changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -98,34 +98,15 @@
     </ul>
   </div>
 </nav>
-        <!-- <form>
-            <table>
-                <br>
-                <tr>
-                    <td><button type="button" style="position : absolute; top: 70px; right:20px;" class="btn btn-sm btn-danger" href="{{ route('signout') }}">Logout</button></td>
-                </tr>  
-            </table>
-        </form> -->
+        
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
 
         <!-- <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth -->
-                        <!-- <a href="{{ url('/home') }}">Home</a> -->
-                        <!-- <a style="position : absolute; top:0; right:0;" class="btn btn-dark" href="{{ route('signout') }}">Logout</a> -->
-
-                    <!-- @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+               
             @endif -->
-            @endif -->
+            @endif
 
     <div class="content">
         <div class="title m-b-md">
@@ -133,18 +114,7 @@
             
         </div>
                 
-                <!-- <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div> -->
-
-            <!-- </div> -->
+        
         </div>
     </body>
 </html>
