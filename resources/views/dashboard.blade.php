5c6be55e7493036f4a56b2007d167ebfe9dcbbe5
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-19 14:59:28 +0530

    t20

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -105,28 +105,29 @@
             </table>
         </form> -->
 
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+        <!-- <div class="flex-center position-ref full-height">
+            @if (Route::has('login')) -->
                 <div class="top-right links">
                     @auth
                         <!-- <a href="{{ url('/home') }}">Home</a> -->
                         <a style="position : absolute; top:0; right:0;" class="btn btn-dark" href="{{ route('signout') }}">Logout</a>
 
-                    @else
+                    <!-- @else
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
+                        @endif -->
+                <div class="content">
                 <div class="title m-b-md">
                     Twenty20 systems
                     
                 </div>
+                    @endauth
+                </div>
+            @endif
+
+            
                 
                 <!-- <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
