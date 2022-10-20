cd88a3744fbd81123a0582457b3539ccbb332e81
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-20 12:03:04 +0530

    form

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -28,11 +28,10 @@ class FormController extends Controller
 
     public function index()
     {
-            // $email = Auth::user()->email;
-            // $data = DB::table('form_basics')->where('email',$email)->latest()->paginate(10);
-            return view('form');
-            // return view('dashboard',compact('data'));
-
+            $email = Auth::user()->email;
+            $data = DB::table('form_basics')->where('email',$email)->latest()->paginate(10);
+            return view('form',compact('data'));
+            
         // print_r($data);
         // return view('form',['form_basics'=>$data]);
 
