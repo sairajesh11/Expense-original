8f83208e207ca4e2276e0844e0d06b67bedccbe5
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-20 17:21:39 +0530

    icon

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,8 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="C:\Users\info\Desktop\form\New folder\favicon.png" type="image/x-icon">
-
-
         <title>Twenty20 systems</title>
 
 
