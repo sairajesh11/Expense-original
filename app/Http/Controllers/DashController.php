52826b9408d092982f80037fd14138d3f15786dc
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-20 16:08:57 +0530

    dash

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -14,7 +14,7 @@ use Auth;
 class DashController extends Controller
 {
     //view page
-    function show()
+    public function index()
     {
             $email = Auth::user()->email;
             $data = DB::table('form_basics')->where('email',$email)->latest()->paginate(10);
