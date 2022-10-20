9f72366543d49be75395536212944c88befa85a7
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-20 11:45:12 +0530

    dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -99,7 +99,7 @@
   </div>
 </nav>
 
-        <div class="flex-center position-ref full-height">
+        <!-- <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
 
             @endif
@@ -108,7 +108,43 @@
         <div class="title m-b-md">
             Twenty20 systems
             
-        </div>
+        </div> -->
+        <br>
+        <h3><center>Expense history</center></h3>
+        <table class="table table-sm table-dark">
+            <thead>
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">Email</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Expense</th>
+                <th scope="col">Amount</th>
+                <th scope="col">Currency</th>
+                <th scope="col">Employee ID</th>
+                <th scope="col">Date of expense</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($data as $key=>$items )
+                <tr>
+                    <td>{{ $data->firstItem() + $key }}</td>
+                    <!-- <th scope="row">{{ ++$key }}</th> -->
+                    <td>{{ $items->email }}</td>
+                    <td>{{ $items->empname }}</td>
+                    <td>{{ $items->phone }}</td>
+                    <td>{{ $items->expense }}</td>
+                    <td>{{ $items->amount }}</td>
+                    <td>{{ $items->currency }}</td>
+                    <td>{{ $items->empid }}</td>
+                    <td>{{ $items->date }}</td>
+                  </tr>
+                
+                @endforeach
+            </tbody>
+          </table>
+          {{$data->links()}}
+
                 
         </div>
     </body>
