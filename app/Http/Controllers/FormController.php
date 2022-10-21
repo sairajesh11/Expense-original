252734f6b614bcf909f41e6c9d6d449c8aab7f37
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-21 15:21:41 +0530

    email

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -74,7 +74,7 @@ class FormController extends Controller
 
                 $saveRecord = [
 
-                    // 'email'=>$request->Auth::user()->email,
+                    'email'=>$credentials = $request->email,
                     // 'empname'=>$request->empname[$key],
                     // 'phone'=>$request->phone[$key],
                     'expense'=>$request->expense[$key],
