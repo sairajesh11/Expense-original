f97cbc82228a446f88f865ec86144586189f81f5
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-25 12:22:12 +0530

    font

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -125,7 +125,7 @@
         <h2  style="font-family: 'POPPINS MEDIUM'; font-size: 25px; ">Expense history</h2>
         <table class="table table-sm" border = "3" style='font-family:"Arial", Courier, monospace; font-size:100%'>
           <!-- <table class="table" border = "8"> -->
-            <thead class="table-dark" font-size ='100%'>
+            <thead class="table-dark" font-size ='110%'>
               <tr>
                 <th scope="col">No</th>
                 <!-- <th scope="col">Email</th>
