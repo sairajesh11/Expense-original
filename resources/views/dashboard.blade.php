6e08ca07730a9b43c5b8f51c69c6181cd288c06e
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-10-25 10:58:12 +0530

    text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -53,7 +53,7 @@
 
             .title {
                 font-size: 30px;
-                position : absolute;  left: 40px; bottom: auto;
+                position : absolute;  left: 20px; bottom: auto;
                 color: black;
                 font-family: Arial;
             }
