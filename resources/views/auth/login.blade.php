dcc4ab79475837bae0bf620363034e66d7e64449
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-11-02 11:57:21 +0530

    center

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,7 +57,7 @@
                               </button>
                           </div>
                           <div class="flex items-center justify-end mt-4">
-                            <a href="{{ url('authorized/google') }}">
+                            <center><a href="{{ url('authorized/google') }}"><center>
                                 <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
                             </a>
                           </div>
