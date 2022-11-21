4634998e07e7048c10840ce01df27ab35e5b3423
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-11-21 19:16:57 +0530

    g

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -16,7 +16,7 @@ class DashController extends Controller
     //view page
     public function index()
     {
-            $email = Auth::user()->email;
+            $email = Auth('middleware')::user()->email;
             $data = DB::table('form_basics')->where('email',$email)->latest()->paginate(10);
             // return formBasic::all();
             return view('dashboard',compact('data'));
