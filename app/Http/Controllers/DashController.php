5e97b0936d941f8d2d5f67c10ef78d03cf9e1071
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-11-21 20:10:40 +0530

    g

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -8,9 +8,9 @@ use Illuminate\support\Facades\DB;
 use App\Models\user;
 use App\Models\formBasic;
 use App\Http\Controllers\CustomAuthController;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+// use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 
 class DashController extends Controller
@@ -18,7 +18,8 @@ class DashController extends Controller
     //view page
     public function index()
     {
-            $email = Auth::user()->email;
+            // $email = Auth::user()->email;
+            $user = User::find(Auth::email());
             $data = DB::table('form_basics')->where('email',$email)->latest()->paginate(10);
             // return formBasic::all();
             return view('dashboard',compact('data'));
