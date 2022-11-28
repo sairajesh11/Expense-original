c1083eb57ae405c9a10b64431170d79f2959cbb8
sairajesh11 <116069968+sairajesh11@users.noreply.github.com>
2022-11-28 12:36:09 +0530

    dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -86,6 +86,33 @@
     </head>
     <body>
 <!-- Navigation Bar -->
+
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <a style="font-family: 'Arial'; font-size: 16px" class="navbar-brand" href="/">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <!-- <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li> -->
+      <li class="nav-item">
+        <a style="font-family: 'Arial'; font-size: 16px;" class="navbar-brand" href="/form">Request form</a>
+      </li>
+      <li class="nav-item">
+        <a style="font-family: 'Arial'; font-size: 16px;" class="navbar-brand" href="/contact">Receipt form</a>
+      </li>
+      <li class="nav-item">
+        <!-- <a class="nav-link " href="/expenses" >Expenses</a> -->
+        <a style="font-family: 'Arial'; font-size: 16px; position : absolute;  right: 80px;" class="navbar-brand" href="{{ route('signout') }}">Sign out</a>
+
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
 <header>
   <!-- Sidebar -->
   <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
